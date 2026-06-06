bdd2482df0d4ddf4c3537f8e304341e38c3a3580
guardadd8 <guardadd@oregonstate.edu>
2026-06-06 17:12:30 -0600

    TODO: ParseTempsFromString, WriteTempsReverse

--- Proj6_guardadd.asm
+++ Proj6_guardadd.asm
@@ -1,21 +1,40 @@
-TITLE Thermometer Readings Program     (Proj6_guardadd.asm)
+TITLE Temperature Reader Program     (Proj6_guardadd.asm)
 
 ; Author: Daniel Guardado
 ; Last Modified: 6/7/2026
 ; OSU email address: guardadd@oregonstate.edu
 ; Course number/section:   CS271 Section 400
 ; Project Number: 6               Due Date: 6/7/2026
-; Description: ***This file is provided as a template from which you may work
-;              when developing assembly projects in CS271.***
+; Description:  This programs asks the user to enter the name of a file that will contain 
+;               comma-delimited temperature values. The program will then open the file, 
+;               read the temperatures, and print the signed values in reverse order.
 
 INCLUDE Irvine32.inc
 
-mGetString MACRO introPromptRef:req, userInputRef:req, maxChars:req, bytesReadRef:req
+
+; -----------------------------------------------------------------
+; Name: mGetString
+;
+; Displays a prompt and reads a string of characters.
+;
+; Preconditions: Do not use EDX or ECX as arguments.
+;
+; Receives:
+;   fileNamePromptRef   = address of text prompt to display
+;   fileNameRef         = address of byte array to store input in
+;   maxChars            = maximum number of characters to read
+;   bytesReadRef        = address variable that stores read length
+;
+; Returns:
+;   fileNameRef         = user typed string
+;   bytesReadRef        = number of bytes read
+; -----------------------------------------------------------------
+mGetString MACRO fileNamePromptRef:req, fileNameRef:req, maxChars:req, bytesReadRef:req
 	pushad
 	
-	mov		edx, introPromptRef
+	mov		edx, fileNamePromptRef
 	call	WriteString
-	mov		edx, userInputRef
+	mov		edx, fileNameRef
 	mov		ecx, maxChars
 	call	ReadString
 	mov		edx, bytesReadRef
@@ -24,6 +43,16 @@ mGetString MACRO introPromptRef:req, userInputRef:req, maxChars:req, bytesReadRe
 	popad
 ENDM
 
+; ---------------------------------------------------------------
+; Name: mDisplayString
+;
+; Prints an ASCII-formatted, null-terminated string to console.
+;
+; Preconditions: stringRef must refer to null-terminated string.
+;
+; Receives:
+;   stringRef   = address of string
+; ---------------------------------------------------------------
 mDisplayString MACRO stringRef:req
 	push	edx
 
@@ -33,6 +62,14 @@ mDisplayString MACRO stringRef:req
 	pop		edx
 ENDM
 
+; ----------------------------------------------------
+; Name: mDisplayChar
+;
+; Displays a single ASCII character to the console.
+;
+; Receives:
+;   charVal     = ASCII character (literal/constant)
+; ----------------------------------------------------
 mDisplayChar MACRO charVal:req
     push	eax
 
@@ -49,61 +86,71 @@ DELIMITER		= ','
 
 .data
 
-	introPrompt		BYTE	"Welcome to the Temperature Value Program. This program will read a comma-delimited file",13,10
+	introPrompt		BYTE	"Welcome to the Temperature Reader Program. This program will read a comma-delimited file",13,10
 					BYTE	"that must be ASCII-formatted. It will retrieve the stored temperature values and reverse",13,10
-					BYTE	"the ordering in order to print them.",13,10,0
+					BYTE	"the ordering to then print them.",13,10,0
 	fileNamePrompt	BYTE	"Enter the name of the file to be read: ",0
 	fileErrorMsg	BYTE	"Incorrect file name.",13,10,0
+    farewellMsg     BYTE    "Goodbye.",0
 
-	fileName		BYTE	MAX_CHARS		DUP(?)
-	bytesRead		DWORD	?
-	fileBuffer		BYTE	BUFFER_SIZE		DUP(?)
-	tempArray       SDWORD  TEMPS_PER_DAY   DUP(?)
-	fileHandle		DWORD	?
+	fileName        BYTE    MAX_CHARS       DUP(?)  ; File name buffer for user string input
+    bytesRead       DWORD   ?                       ; Length of user string input
+    fileBuffer      BYTE    BUFFER_SIZE     DUP(?)  ; Buffer for raw text contents from file
+    tempArray       SDWORD  TEMPS_PER_DAY   DUP(?)  ; Array for storing converted signed integers
+    fileHandle      DWORD   ?                       ; System handle variable for the opened file
 
 .code
 main PROC
-	mDisplayString	OFFSET introPrompt
-	mGetString		OFFSET fileNamePrompt, OFFSET fileName, MAX_CHARS, OFFSET bytesRead
+; Display introduction
+    mDisplayString  OFFSET introPrompt
 
-	mov		edx, OFFSET fileName
-	call	OpenInputFile
-	mov		fileHandle, eax
+_promptUser:
+    ; Get user filename, attempt to open file.
+    mGetString      OFFSET fileNamePrompt, OFFSET fileName, MAX_CHARS, OFFSET bytesRead
+    mov             edx, OFFSET fileName
+    call            OpenInputFile
+    mov             fileHandle, eax
 
-	cmp		eax, INVALID_HANDLE_VALUE
-	je		_invalidFile
-	
-	mov		eax, fileHandle
-    mov		edx, OFFSET fileBuffer
-    mov		ecx, BUFFER_SIZE
-    call	ReadFromFile
-	mov		eax, fileHandle
-    call	CloseFile
+    ; Check file handle. If negative, throw error message and reprompt.
+    cmp             eax, INVALID_HANDLE_VALUE
+    jne             _fileValid
+    mDisplayString  OFFSET fileErrorMsg
+    jmp             _promptUser
 
-    push    OFFSET fileBuffer
-    push    OFFSET tempArray
-    call    ParseTempsFromString
+_fileValid:
+    ; Read and store text data stream into fileBuffer. Close file stream after finished.
+    mov             eax, fileHandle
+    mov             edx, OFFSET fileBuffer
+    mov             ecx, BUFFER_SIZE
+    call            ReadFromFile
+    mov             eax, fileHandle
+    call            CloseFile
 
-    push	OFFSET tempArray
-	call	WriteTempsReverse
-	
-    jmp		_exit
+    ; Convert fileBuffer content into formatted SDWORD values.
+    push            OFFSET fileBuffer
+    push            OFFSET tempArray
+    call            ParseTempsFromString
 
-_invalidFile:
-	mDisplayString		OFFSET fileErrorMsg
+    ; Print converted signed array items in reverse order.
+    push            OFFSET tempArray
+    call            WriteTempsReverse
+
+    ; Display farewell message.
+    call            CrLf
+    mDisplayString  OFFSET farewellMsg
 
 _exit:
-	Invoke ExitProcess,0
+    Invoke  ExitProcess,0
 main ENDP
 
 ParseTempsFromString PROC
     LOCAL   charBuffer[8]:BYTE, negativeFlag:DWORD
     pushad
 
-    mov     esi, [ebp + 12]     ; ESI = pointer to fileBuffer string
-    mov     edi, [ebp + 8]      ; EDI = pointer to tempArray destination
+    mov     esi, [ebp + 12]     ; fileBuffer string pointer
+    mov     edi, [ebp + 8]      ; tempArray destination pointer
     
-    mov     ecx, TEMPS_PER_DAY  ; Counter loop for exactly 24 elements
+    mov     ecx, TEMPS_PER_DAY
 
 _collectLoop:
     ; Set up destination pointer to our local temporary buffer manually without LEA
@@ -213,7 +260,7 @@ _reverseLoop:
     sub     esi, 4              
     loop    _reverseLoop        ; Decrement ECX and repeat until 24 elements are done
 
-    call    Crlf                ; Drop to a new line at the very end of the line output
+    call    CrLf                ; Drop to a new line at the very end of the line output
     
     popad
     pop     ebp
